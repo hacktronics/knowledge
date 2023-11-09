3990dad79a4ba24f1875ec2e99388c90a0b962cd
Hacktronics India <support@hacktronics.co.in>
2023-11-09 23:37:50 +0530

    new script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
-const imgs = document.getElementsByClassName("blocks");
-for (let i = 0; i < imgs.length; i++) {
-  let h = imgs[i].height;
-  h = h * 0.5;
-  imgs[i].height = h;
-}
+window.addEventListener("load", () => {
+  var imgs = document.images;
+  var len = imgs.length;
+
+  [].forEach.call(imgs, function(img) {
+    if(img.complete)
+      resizeImage(img);
+    else
+      img.addEventListener('load', () => resizeImage(img), false);
+  });
+
+  function resizeImage(img) {
+    let h = img.height;
+    h = h * 0.5;
+    img.height = h;
+  }
+});
